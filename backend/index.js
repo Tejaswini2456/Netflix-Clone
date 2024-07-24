5d45b318c269d27bb08b7bc66cfa082b5f28acd8
Tejaswini2456 <151370646+Tejaswini2456@users.noreply.github.com>
2024-07-24 14:32:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://netflix-clone-frontend-eight.vercel.app/',
+    origin:'http://localhost:3000',
     credentials:true
 }
 app.use(cors(corsOptions));
