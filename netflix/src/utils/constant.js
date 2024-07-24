9301a03a97dc08cc161b2da6e9a29e4d861a8d97
Tejaswini2456 <151370646+Tejaswini2456@users.noreply.github.com>
2024-07-24 14:35:02 +0530

    Update constant.js

--- netflix/src/utils/constant.js
+++ netflix/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const API_END_POINT = "https://netflix-clone-backend-dz3g.onrender.com";
+export const API_END_POINT = "http://localhost:8080/api/v1/user";
 
 export const options = {
     method: 'GET',
